ae0f30ebe56ca5423080a923a7bf84df5f2bd20b
Rohan-cherkar <cherkarrohan@gmail.com>
2026-01-07 09:37:35 +0530

    Change var to let for searchBox declaration

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const apiKey="0816c7fc83803336c46424d250dac9ce";
 const apiURL="https://api.openweathermap.org/data/2.5/weather?units=metric"
-var searchBox=document.querySelector("#search");
+let searchBox=document.querySelector("#search");
 const srcButton=document.querySelector(".button");
 async function GetData(city){
     const response= await fetch(apiURL +`&q=${city}&appid=${apiKey}`);
